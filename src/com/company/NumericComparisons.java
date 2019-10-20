0a74d3ebda66948c84a97afb89dcb9f70dab0c76
Ariannarich <arianna.richardson@ssfs.org>
2019-10-20 18:54:57 -0400

    Numeric Comparisons done

--- src/com/company/NumericComparisons.java
+++ src/com/company/NumericComparisons.java
@@ -4,11 +4,22 @@ import java.util.Scanner;
 
 public class NumericComparisons {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        System.out.println(num + " is odd: "  + isOdd(num));
+        System.out.println(num + " is even: "  + isEven(num));
+        System.out.println(num + " is zero: "  + isZero(num));
+        System.out.println(num + " is positive: "  + isPositive(num));
+        System.out.println(num + " is negative: "  + isNegative(num));
+        System.out.println(num + " is non negative: "  + isNonNegative(num));
+        System.out.println(num + " is non positive: "  + isNonPositive(num));
     }
 
     public static boolean isOdd(int num) {
+
         return num % 2 != 0;
+
     }
 
     public static boolean isEven(int num) {
@@ -24,6 +35,14 @@ public class NumericComparisons {
     }
 
     public static boolean isNegative(int num) {
-        return num <= 0;
+        return num  < 0;
+    }
+    public static boolean isNonNegative(int num) {
+        return num  <= 0;
+    }
+    public static boolean isNonPositive(int num) {
+        return num  >= 0;
     }
 }
+
+
