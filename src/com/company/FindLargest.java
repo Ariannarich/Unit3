21e3628fdc60df1f714695eee6bce1a8cbc66582
richaar <arianna.richardson@ssfs.org>
2019-10-21 12:09:05 -0400

    Find Largest done

--- src/com/company/FindLargest.java
+++ src/com/company/FindLargest.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class FindLargest {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int num1 = input.nextInt();
+        System.out.println("Enter second number:");
+        int num2 = input.nextInt();
+
+        if (num1==num2)
+                System.out.println("The numbers are equal");
+        if (num1 > num2)
+            System.out.println(num1 + " is greater than " +num2);
+        if (num2 > num1)
+            System.out.println(num2+" is greater than " + num1);
+        }
+
+
 }
