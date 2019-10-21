e3e07c2d07d5223c0d1206858b6c1200f3590096
richaar <arianna.richardson@ssfs.org>
2019-10-21 11:59:48 -0400

    Numeric Comparisons part two

--- src/com/company/NumericComparisons.java
+++ src/com/company/NumericComparisons.java
@@ -6,14 +6,22 @@ public class NumericComparisons {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter number:");
         int num = input.nextInt();
-        System.out.println(num + " is odd: "  + isOdd(num));
-        System.out.println(num + " is even: "  + isEven(num));
-        System.out.println(num + " is zero: "  + isZero(num));
-        System.out.println(num + " is positive: "  + isPositive(num));
-        System.out.println(num + " is negative: "  + isNegative(num));
-        System.out.println(num + " is non negative: "  + isNonNegative(num));
-        System.out.println(num + " is non positive: "  + isNonPositive(num));
+
+        System.out.println(num + " is odd: " + isOdd(num));
+        System.out.println(num + " is even: " + isEven(num));
+        System.out.println(num + " is zero: " + isZero(num));
+        System.out.println(num + " is positive: " + isPositive(num));
+        System.out.println(num + " is negative: " + isNegative(num));
+        System.out.println(num + " is non negative: " + isNonNegative(num));
+        System.out.println(num + " is non positive: " + isNonPositive(num));
+      // Part 2
+    System.out.println("Enter second number:");
+        int num2 = input.nextInt();
+        System.out.println(num + " is a multiple of " + num2 +": "  + isMultiple(num,num2));
+        System.out.println(num + " is a factor of " + num2 +": " + isFactor(num,num2));
+
     }
 
     public static boolean isOdd(int num) {
@@ -35,14 +43,24 @@ public class NumericComparisons {
     }
 
     public static boolean isNegative(int num) {
-        return num  < 0;
+        return num < 0;
     }
+
     public static boolean isNonNegative(int num) {
-        return num  >= 0;
+        return num >= 0;
     }
+
     public static boolean isNonPositive(int num) {
-        return num  <= 0;
+        return num <= 0;
+    }
+
+    // Part 2
+    public static boolean isMultiple(int num1, int num2) {
+        return num1 % num2 == 0;
+    }
+
+    public static boolean isFactor(int num1, int num2) {
+        return num1 / 5 == 0;
     }
 }
-
 
