1788b02e7383dec805f420e5fb70eca2cdd14d66
richaar <arianna.richardson@ssfs.org>
2019-10-23 14:40:02 -0400

    Slope of a line done

--- src/com/company/FindLargest.java
+++ src/com/company/FindLargest.java
@@ -6,8 +6,11 @@ public class FindLargest {
     public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
         System.out.println("Enter first number:");
+
         int num1 = input.nextInt();
+
         System.out.println("Enter second number:");
+
         int num2 = input.nextInt();
 
         if (num1==num2)
--- src/com/company/SlopeOfALine.java
+++ src/com/company/SlopeOfALine.java
@@ -3,28 +3,41 @@ package com.company;
 import java.util.Scanner;
 
 public class SlopeOfALine {
-    public static void main(String[] args) {
+
+
+        public static String slope(int x1, int y1, int x2, int y2)
+        {
+
+            if (x2 - x1 == 0) {
+              return ("The line defined by the points (" + x1 + "," + y1 +") and (" + x2 + "," + y2 + ") is a vertical line and the slope is undefined." );
+
+            }
+            else
+            {
+                double changeY = y2 - y1;
+                double changeX = x2 - x1;
+                double slope = changeY /changeX;
+               return ("The line defined by the points (" + x1 + "," + y1 +") and (" + x2 + "," + y2 + ") has a slope of " + slope );
+
+
+            }
+
+            }
+
+public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter x1:");
         int x1 = input.nextInt();
         System.out.println("Enter y1:");
-        int y1 = input.nextInt();
+    int y1 = input.nextInt();
         System.out.println("Enter x2:");
-        int x2 = input.nextInt();
+    int x2 = input.nextInt();
         System.out.println("Enter y2:");
-        int y2 = input.nextInt();
+    int y2 = input.nextInt();
+    System.out.println(slope(x1, y1, x2, y2));
 
-        if (x2 - x1 == 0) {
-            System.out.println("The line defined by the points (" + x1 + "," + y1 +") and (" + x2 + "," + y2 + ") is a vertical line and the slope is undefined." );
-        }
-        else
-        {
-            int changeY = y2 - y1;
-            int changeX = x2 - x1;
-            double slope = changeY /changeX;
-            System.out.println("The line defined by the points (" + x1 + "," + y1 +") and (" + x2 + "," + y2 + ") has a slope of " + slope );
-        }
-        }
+
+}
 
 
     }
